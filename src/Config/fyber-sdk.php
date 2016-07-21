536ddbdd09cd7ca55f70c25e136b4ea776be15f5
Mahmoud Zalt <mahmoud.z@msn.com>
2016-07-21 23:37:56 +0300

    add a config file

---
+++ src/Config/fyber-sdk.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'api_key'         => '',
+
+    'android_app_id'  => '',
+    'ios_app_id'      => '',
+    'web_app_id'      => '',
+
+    'base_url'        => 'http://api.fyber.com/feed/',
+    'api_version'     => 1,
+    'response_format' => 'json',
+
+];
