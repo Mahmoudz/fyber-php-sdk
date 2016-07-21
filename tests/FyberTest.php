346a17f1c70f9d371d737960c44a441cc6d31fd5
Mahmoud Zalt <mahmoud.z@msn.com>
2016-07-21 23:37:48 +0300

    prepare a test directory

---
+++ tests/FyberTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace mahmoudz\fyberPhpSdk\Tests;
+
+use mahmoudz\fyberPhpSdk\Say;
+
+/**
+ * Class FyberTest
+ *
+ * @author  Mahmoud Zalt  <mahmoud@zalt.me>
+ */
+class FyberTest extends TestCase
+{
+
+    public function testGetOffers()
+    {
+        $this->assertTrue(true);
+    }
+
+
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+namespace mahmoudz\fyberPhpSdk\Tests;
+
+use Nextpack\Library\AbstractTestCase;
+use Mockery as M;
+
+/**
+ * Class TestCase is the parent class of all Tests in the package.
+ * Every Test must extend from this TestCase.
+ *
+ * @category Parent TestCase
+ * @package mahmoudz\fyberPhpSdk\Tests
+ * @author  Mahmoud Zalt <mahmoud@zalt.me>
+ */
+class TestCase extends AbstractTestCase
+{
+
+}
