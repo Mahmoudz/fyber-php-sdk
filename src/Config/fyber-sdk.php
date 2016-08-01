b2ee400a57289b1e9730c82b3c0597c65a5c1de7
Mahmoud Zalt <mahmoud.z@msn.com>
2016-08-01 18:14:52 +0300

    add offer_callback_token to the config file

--- src/Config/fyber-sdk.php
+++ src/Config/fyber-sdk.php
@@ -2,14 +2,16 @@
 
 return [
 
-    'api_key'         => env('FYBER_KEY', ''),
+    'api_key' => env('FYBER_KEY', ''),
 
-    'android_app_id'  => env('ANDROID_APP_ID', ''),
-    'ios_app_id'      => env('IOS_APP_ID', ''),
-    'web_app_id'      => env('WEB_APP_ID', ''),
+    'android_app_id' => env('ANDROID_APP_ID', ''),
+    'ios_app_id'     => env('IOS_APP_ID', ''),
+    'web_app_id'     => env('WEB_APP_ID', ''),
 
     'base_url'        => 'http://api.fyber.com/feed/',
     'api_version'     => '1',
     'response_format' => 'json',
+
+    'offer_callback_token' => env('FYBER_OFFER_CALLBACK_TOKEN', ''),
 
 ];
